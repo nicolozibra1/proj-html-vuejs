799db011d3eb88d7f0f74942bfb541f7449c86a9
nicolozibra1 <nico.zibra@gmail.com>
2023-04-26 11:47:06 +0200

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  
+</template>
+
+<script>
+import {store} from './data/store.js'
+  export default {
+    name: 'App',
+    data() {
+      return {
+        store,
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
